9d6928a787004c9ddfcc665ec46f142f744711b0
agustindavidnievas <agustindavidnievas90@gmail.com>
2018-04-29 01:43:16 -0300

    evaluacion del modelo

--- test/testData.py
+++ test/testData.py
@@ -1,6 +1,9 @@
 import os
 import tensorflow as tf
+import tensorflow.contrib.eager as tfe
 from ..src.parseData import parse_csv
+
+tf.enable_eager_execution()
 
 test_url = "http://download.tensorflow.org/data/iris_test.csv"
 
@@ -14,3 +17,17 @@ test_dataset = test_dataset.shuffle(1000)       # randomize
 test_dataset = test_dataset.batch(32)           # use the same batch size as the training set
 
 print("Local copy of the datatest file: {}".format(test_fp))
+
+model = tf.keras.Sequential([#see this numbers...
+  tf.keras.layers.Dense(10, activation="relu", input_shape=(4,)),  # input shape required
+  tf.keras.layers.Dense(10, activation="relu"),
+  tf.keras.layers.Dense(3)
+])
+
+test_accuracy = tfe.metrics.Accuracy()
+
+for (x, y) in tfe.Iterator(test_dataset):
+  prediction = tf.argmax(model(x), axis=1, output_type=tf.int32)
+  test_accuracy(prediction, y)
+
+print("Test set accuracy: {:.3%}".format(test_accuracy.result()))
